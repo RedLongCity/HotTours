644596ae08ce713eef9aa07d71ef8c986bae2960
Vladimir_Kharchenko <RedLongCity@gmail.com>
2017-10-09 12:45:52 +0300

    TourDetailPresenter Done

--- app/src/main/java/com/smitsworks/redlo/hottours/tourdetails/TourDetailPresenter.java
+++ app/src/main/java/com/smitsworks/redlo/hottours/tourdetails/TourDetailPresenter.java
@@ -3,13 +3,19 @@ package com.smitsworks.redlo.hottours.tourdetails;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.smitsworks.redlo.hottours.data.models.Hotel_Image;
+import com.smitsworks.redlo.hottours.data.models.Price;
 import com.smitsworks.redlo.hottours.data.models.Tour;
 import com.smitsworks.redlo.hottours.data.source.ToursDataSource;
 import com.smitsworks.redlo.hottours.data.source.ToursRepository;
+import com.smitsworks.redlo.hottours.tours.TourCurrencyType;
 
 import java.sql.Date;
+import java.util.HashSet;
+import java.util.Iterator;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.smitsworks.redlo.hottours.tours.TourCurrencyType.*;
 
 /**
  * Created by redlongcity on 08.10.2017.
@@ -23,12 +29,16 @@ public class TourDetailPresenter implements TourDetailsContract.Presenter {
 
     private final TourDetailsContract.View tourDetailsView;
 
+    private TourCurrencyType currencyType;
+
     @Nullable
     private Integer tourId;
 
     public TourDetailPresenter(@NonNull Integer tourId,
+                               @NonNull TourCurrencyType requestType,
                                @NonNull ToursRepository toursRepository,
                                @NonNull TourDetailsContract.View tourView) {
+        this.currencyType = checkNotNull(requestType);
         this.tourId=tourId;
         this.toursRepository = checkNotNull(toursRepository);
         this.tourDetailsView = checkNotNull(tourView);
@@ -78,8 +88,18 @@ public class TourDetailPresenter implements TourDetailsContract.Presenter {
         });
     }
 
+    @Override
+    public void setCurrencyType(TourCurrencyType requestType) {
+        currencyType=requestType;
+    }
+
+    @Override
+    public TourCurrencyType getCurrencyType() {
+        return currencyType;
+    }
+
     private void showTour(@NonNull Tour tour){
-        String countryName;
+        String countryName=null;
 
         if (tour.getCountry() != null) {
             countryName = tour.getCountry().getName();
@@ -87,13 +107,13 @@ public class TourDetailPresenter implements TourDetailsContract.Presenter {
 
         String region = tour.getRegion();
         String hotelName = tour.getHotel();
-        String hotelRating;
+        String hotelRating=null;
 
         if (tour.getHotel_Rating() != null) {
             hotelRating = tour.getHotel_Rating().getName();
         }
 
-        String mealType;
+        String mealType=null;
 
         if (tour.getMeal_Type() != null) {
             mealType=tour.getMeal_Type().getName_full();
@@ -103,7 +123,126 @@ public class TourDetailPresenter implements TourDetailsContract.Presenter {
         Integer childrenAmount = tour.getChild_Amount();
         Integer duration = tour.getDuration();
         Date dateFrom = tour.getDate_From();
+        Integer priceValue=null;
+        String currencySymbol = null;
+        String currencyId=null;
+        switch (getCurrencyType()){
+            case DOLLAR:
+                currencyId = "1";
+                currencySymbol = "$";
+                break;
+            case HRYVNA:
+                currencyId="2";
+                currencySymbol="грн";
+                break;
+            case EURO:
+                currencyId = "10";
+                currencySymbol="€";
+                break;
+        }
+        HashSet<Price> priceHashSet = (HashSet<Price>) tour.getPrices();
+        Iterator<Price> it = priceHashSet.iterator();
+        while(it.hasNext()){
+            Price price = it.next();
+            if(price.getCurrency().getId().equals(currencyId)){
+                priceValue=price.getCost();
+            }
+        }
+        String fromCity = tour.getFrom_Cities().getName();
+        String transportType = tour.getTransport_Type();
+        String hotelImageURL = null;
+        HashSet<Hotel_Image> imageSet = (HashSet<Hotel_Image>) tour.getHotel_ImageSet();
+        if(imageSet!=null){
+            Iterator<Hotel_Image> iterator = imageSet.iterator();
+            while (iterator.hasNext()){
+                Hotel_Image image = iterator.next();
+                hotelImageURL = image.getFull();
+            }
+        }
 
+        if (countryName != null) {
+            tourDetailsView.showCountryName(countryName);
+        }else{
+            tourDetailsView.hideCountryName();
+        }
+
+        if (region != null) {
+            tourDetailsView.showRegion(region);
+        }else{
+            tourDetailsView.hideRegion();
+        }
+
+        if(hotelName!=null){
+            tourDetailsView.showHotelName(hotelName);
+        }else{
+            tourDetailsView.hideHotelName();
+        }
+
+        if (hotelRating != null) {
+            tourDetailsView.showHotelRating(hotelRating);
+        }else{
+            tourDetailsView.hideHotelRating();
+        }
+
+        if (mealType != null) {
+            tourDetailsView.showMealType(mealType);
+        }else{
+            tourDetailsView.hideMealType();
+        }
+
+        if (adultAmount != null) {
+            tourDetailsView.showAdultAmount(adultAmount);
+        }else{
+            tourDetailsView.hideAdultAmount();
+        }
+
+        if (childrenAmount != null) {
+            tourDetailsView.showChildrenAmount(childrenAmount);
+        }else{
+            tourDetailsView.hideChildrenAmount();
+        }
+
+        if (duration != null) {
+            tourDetailsView.showDuration(duration);
+        }else{
+            tourDetailsView.hideDuration();
+        }
+
+        if (dateFrom != null) {
+            tourDetailsView.showDateFrom(dateFrom);
+        }else{
+            tourDetailsView.hideDateFrom();
+        }
+
+        if (priceValue != null) {
+            tourDetailsView.showPriceValue(priceValue);
+        }else{
+            tourDetailsView.hidePriceValue();
+        }
+
+        if (currencySymbol != null) {
+            tourDetailsView.showCurrencySymbol(currencySymbol);
+        }else{
+            tourDetailsView.hideCurrencySymbol();
+        }
+
+        if (fromCity != null) {
+            tourDetailsView.showFromCity(fromCity);
+        }else{
+            tourDetailsView.hideFromCity();
+        }
+
+        if (transportType != null) {
+            tourDetailsView.showTransportType(transportType);
+        }else{
+            tourDetailsView.hideTransportType();
+        }
+
+        if (hotelImageURL != null) {
+            tourDetailsView.showImage(hotelImageURL);
+        }else{
+            tourDetailsView.hideImage();
+        }
 
     }
 }
--- app/src/main/java/com/smitsworks/redlo/hottours/tourdetails/TourDetailsContract.java
+++ app/src/main/java/com/smitsworks/redlo/hottours/tourdetails/TourDetailsContract.java
@@ -1,7 +1,11 @@
 package com.smitsworks.redlo.hottours.tourdetails;
 
+import com.google.common.base.Strings;
 import com.smitsworks.redlo.hottours.BasePresenter;
 import com.smitsworks.redlo.hottours.BaseView;
+import com.smitsworks.redlo.hottours.tours.TourCurrencyType;
+
+import java.sql.Date;
 
 /**
  * Created by redlongcity on 08.10.2017.
@@ -16,6 +20,62 @@ public interface TourDetailsContract {
 
         void showMissingTour();
 
+        void showCountryName(String countryName);
+
+        void hideCountryName();
+
+        void showRegion(String region);
+
+        void hideRegion();
+
+        void showHotelName(String hotelName);
+
+        void hideHotelName();
+
+        void showHotelRating(String hotelRating);
+
+        void hideHotelRating();
+
+        void showMealType(String mealType);
+
+        void hideMealType();
+
+        void showAdultAmount(Integer adultsAmount);
+
+        void hideAdultAmount();
+
+        void showChildrenAmount(Integer childAmount);
+
+        void hideChildrenAmount();
+
+        void showDuration(Integer duration);
+
+        void hideDuration();
+
+        void showDateFrom(Date dateFrom);
+
+        void hideDateFrom();
+
+        void showPriceValue(Integer priceValue);
+
+        void hidePriceValue();
+
+        void showCurrencySymbol(String currencySymbol);
+
+        void hideCurrencySymbol();
+
+        void showFromCity(String city);
+
+        void hideFromCity();
+
+        void showTransportType(String transportType);
+
+        void hideTransportType();
+
+        void showImage(String imageUrl);
+
+        void hideImage();
+
         void showTour(Integer tourId);
 
         boolean isActive();
@@ -25,6 +85,10 @@ public interface TourDetailsContract {
     interface Presenter extends BasePresenter{
 
         void loadTour(Integer tourId);
+
+        void setCurrencyType(TourCurrencyType requestType);
+
+        TourCurrencyType getCurrencyType();
 
     }
 
