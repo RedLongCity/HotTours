3cdd158d7cc021ca3f63c255d39019ea865e2998
Vladimir_Kharchenko <RedLongCity@gmail.com>
2017-10-20 23:38:48 +0300

    Started HotelRatings Module

---
+++ app/src/main/java/com/smitsworks/redlo/hottours/lists/hotelratings/HotelRatingsContract.java
@@ -0,0 +1,41 @@
+package com.smitsworks.redlo.hottours.lists.hotelratings;
+
+import android.support.annotation.NonNull;
+
+import com.smitsworks.redlo.hottours.BasePresenter;
+import com.smitsworks.redlo.hottours.BaseView;
+import com.smitsworks.redlo.hottours.data.models.Country;
+import com.smitsworks.redlo.hottours.data.models.Hotel_Rating;
+
+import java.util.List;
+
+/**
+ * Created by redlongcity on 20.10.2017.
+ * This interface declares the relationships between the view and the presenter
+ */
+
+public interface HotelRatingsContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setLoadingIndicator(boolean active);
+
+        void showLoadingHotelRatingsError();
+
+        void showHotelRatings(List<Hotel_Rating> ratings);
+
+        void showNoHotelRatings();
+
+        void chooseHotelRatingUI(Hotel_Rating rating);
+
+        boolean isActive();
+    }
+
+    interface Presenter extends BasePresenter{
+
+        void loadHotelRatings(boolean forceUpdate);
+
+        void chooseHotelRating(@NonNull Hotel_Rating rating);
+
+    }
+}
---
+++ app/src/main/java/com/smitsworks/redlo/hottours/lists/hotelratings/HotelRatingsPresenter.java
@@ -0,0 +1,28 @@
+package com.smitsworks.redlo.hottours.lists.hotelratings;
+
+
+import android.support.annotation.NonNull;
+
+import com.smitsworks.redlo.hottours.data.models.Hotel_Rating;
+
+/**
+ * Created by redlongcity on 20.10.2017.
+ * Listens to user actions from the UI, retrieves the data and upgrade the UI
+ */
+
+public class HotelRatingsPresenter implements HotelRatingsContract.Presenter {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loadHotelRatings(boolean forceUpdate) {
+
+    }
+
+    @Override
+    public void chooseHotelRating(@NonNull Hotel_Rating rating) {
+
+    }
+}
